00f31589c9aaab27844c03b21bcc1d790f5e5dec
Jacob <jacobshorterivey@gmail.com>
2019-12-08 01:23:11 -0700

    got create to add item to table and reload the table

--- EventTracker/src/main/resources/static/js/script.js
+++ EventTracker/src/main/resources/static/js/script.js
@@ -1,11 +1,29 @@
-window.addEventListener('load', function(e){
+window.addEventListener('load', function(e) {
 	console.log("Document loaded");
 	getAllEvents();
 	init();
 });
 
-function init(){
-	//TODO 
+function init() {
+	document.eventForm.lookup.addEventListener('click', function(e) {
+		e.preventDefault();
+		var eventId = document.eventForm.eventId.value;
+		if (!isNaN(eventId) && eventId > 0) {
+			getEvent(eventId);
+		}
+	});
+	document.addEventForm.create.addEventListener('click', function(e) {
+		e.preventDefault();
+		addNewEvent();
+	});
+}
+
+function clearEventDataDiv() {
+//traverse adn select eventData div and clear it out
+	let eventDataDiv = document.getElementById('eventData');
+	while (eventDataDiv.firstElementChild) {
+		eventDataDiv.removeChild(eventDataDiv.firstElementChild);
+	}
 }
 
 function addNewEvent() {
@@ -16,6 +34,8 @@ function addNewEvent() {
 		if (xhr.readyState === 4 && xhr.status < 400) {
 			var eventObject = JSON.parse(xhr.responseText);
 			displayEvent(eventObject);
+			clearEventDataDiv();
+			getAllEvents();
 		}
 		if (xhr.readyState === 4 && xhr.status >= 400) {
 			console.error(xhr.status + ': ' + xhr.responseText);
@@ -34,21 +54,15 @@ function addNewEvent() {
 		hoursToComplete : form.hoursToComplete.value,
 		imgUrl : form.imgUrl.value,
 		genre : form.genre.value,
-		
-//		title : form.title.value,
-//		description : form.description.value,
-//		releaseYear : form.releaseYear.value,
-//		rating : form.rating.value,
-//		length : form.length.value,
-//		replacementCost : 5.99
 	};
-	var newEventJsonString = JSON.stringify(newEventObject); // Convert JS object to
-	// JSON string
+
+	var newEventJsonString = JSON.stringify(newEventObject); // Convert JS object to JSON string
 	xhr.send(newEventJsonString);
+
 }
 
-function getAllEvents(){
-	//TODO 
+function getAllEvents() {
+	// TODO
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'http://localhost:8090/api/games/', true);
 	xhr.onreadystatechange = function() {
@@ -70,127 +84,128 @@ function displayAllEvents(eventList) {
 	// TODO:
 	// * Create and append elements to the data div to display:
 	dataDiv.textContent = '';
-//	while (dataDiv.firstElementChild) {
-//		dataDiv.removeChild(dataDiv.firstElementChild);
-//	}
+	while (dataDiv.firstElementChild) {
+		dataDiv.removeChild(dataDiv.firstElementChild);
+	}
 	let table = document.createElement('table');
 
-	  //table heads
-	  let tableHead1 = document.createElement('th');
-//	  let tableHead2 = document.createElement('th');
-	  let tableHead3 = document.createElement('th');
-	  let tableHead4 = document.createElement('th');
-	  let tableHead5 = document.createElement('th');
-	  let tableHead6 = document.createElement('th');
-	  let tableHead7 = document.createElement('th');
-	  let tableHead8 = document.createElement('th');
-	  let tableHead9 = document.createElement('th');
-	  
-	  tableHead1.textContent = 'Name';
-//	  tableHead2.textContent = 'Description';
-	  tableHead3.textContent = 'Developer';
-	  tableHead4.textContent = 'Platform';
-	  tableHead5.textContent = 'Release Date';
-	  tableHead6.textContent = 'Completed';
-	  tableHead7.textContent = 'Hours To Complete';
-	  tableHead8.textContent = 'Image URL';
-	  tableHead9.textContent = 'Genre';
-	  
-	  table.appendChild(tableHead1);
-//	  table.appendChild(tableHead2);
-	  table.appendChild(tableHead3);
-	  table.appendChild(tableHead4);
-	  table.appendChild(tableHead5);
-	  table.appendChild(tableHead6);
-	  table.appendChild(tableHead7);
-	  table.appendChild(tableHead8);
-	  table.appendChild(tableHead9);
-	
-	for (let i = 0; i < eventList.length; i++) {
-		  
-		  //table data
-		  let tableRow = document.createElement('tr');
-		  table.appendChild(tableRow);
-		  
-		  let tableData1 = document.createElement('td');
-//		  let tableData2 = document.createElement('td');
-		  let tableData3 = document.createElement('td');
-		  let tableData4 = document.createElement('td');
-		  let tableData5 = document.createElement('td');
-		  let tableData6 = document.createElement('td');
-		  let tableData7 = document.createElement('td');
-		  let tableData8 = document.createElement('td');
-		  let tableData9 = document.createElement('td');
-		  tableData1.textContent = eventList[i].name;
-//		  tableData2.textContent = eventList[i].description;
-		  tableData3.textContent = eventList[i].developer;
-		  tableData4.textContent = eventList[i].platform;
-		  tableData5.textContent = eventList[i].releaseDate;
-		  tableData6.textContent = eventList[i].completed;
-		  tableData7.textContent = eventList[i].hoursToComplete;
-		  tableData8.textContent = eventList[i].imgUrl;
-		  tableData9.textContent = eventList[i].genre;
-		  tableRow.appendChild(tableData1);
-//		  tableRow.appendChild(tableData2);
-		  tableRow.appendChild(tableData3);
-		  tableRow.appendChild(tableData4);
-		  tableRow.appendChild(tableData5);
-		  tableRow.appendChild(tableData6);
-		  tableRow.appendChild(tableData7);
-		  tableRow.appendChild(tableData8);
-		  tableRow.appendChild(tableData9);
-		}
+	// table heads
+	let tableHead1 = document.createElement('th');
+	// let tableHead2 = document.createElement('th');
+	let tableHead3 = document.createElement('th');
+	let tableHead4 = document.createElement('th');
+	let tableHead5 = document.createElement('th');
+	let tableHead6 = document.createElement('th');
+	let tableHead7 = document.createElement('th');
+	let tableHead8 = document.createElement('th');
+	let tableHead9 = document.createElement('th');
 
-		document.body.appendChild(table);
-	
-//	for(let i = 0; i < eventList.length; i++){
-//		let eventH2 = document.createElement('h2');
-//		dataDiv.appendChild(eventH2);
-//		eventH2.textContent = eventList[i].name;
-//		
-//		let descriptionBq = document.createElement('blockquote');
-//		dataDiv.appendChild(descriptionBq);
-//		descriptionBq.textContent = eventList[i].description;
-//		
-//		let miscUl = document.createElement('ul');
-//		dataDiv.appendChild(miscUl);
-//		
-//		let developerLi = document.createElement('li');
-//		miscUl.appendChild(developerLi);
-//		developerLi.textContent = 'Developer: ' + eventList[i].developer;
-//		
-//		let platformLi = document.createElement('li');
-//		miscUl.appendChild(platformLi);
-//		platformLi.textContent = 'Platform: ' + eventList[i].platform;
-//		
-//		let releaseDateLi = document.createElement('li');
-//		miscUl.appendChild(releaseDateLi);
-//		releaseDateLi.textContent = 'Release Date: ' + eventList[i].releaseDate;
-//		
-//		let completedLi = document.createElement('li');
-//		miscUl.appendChild(completedLi);
-//		if(eventList[i].completed){
-//			completedLi.textContent = 'Completed: Yes';
-//		}
-//		else{
-//			completedLi.textContent = 'Completed: No';
-//
-//		}
-//		
-//		let hoursToCompleteLi = document.createElement('li');
-//		miscUl.appendChild(hoursToCompleteLi);
-//		hoursToCompleteLi.textContent = 'Hours To Complete: ' + eventList[i].hoursToComplete;
-//		
-//		let imgUrlLi = document.createElement('li');
-//		miscUl.appendChild(imgUrlLi);
-//		imgUrlLi.textContent = 'IMG URL: ' + eventList[i].imgUrl;
-//		
-//		let genreLi = document.createElement('li');
-//		miscUl.appendChild(genreLi);
-//		genreLi.textContent = 'Genre: ' + eventList[i].genre;
-//		
-//	}
-	
+	tableHead1.textContent = 'Name';
+	// tableHead2.textContent = 'Description';
+	tableHead3.textContent = 'Developer';
+	tableHead4.textContent = 'Platform';
+	tableHead5.textContent = 'Release Date';
+	tableHead6.textContent = 'Completed';
+	tableHead7.textContent = 'Hours To Complete';
+	tableHead8.textContent = 'Image URL';
+	tableHead9.textContent = 'Genre';
+
+	table.appendChild(tableHead1);
+	// table.appendChild(tableHead2);
+	table.appendChild(tableHead3);
+	table.appendChild(tableHead4);
+	table.appendChild(tableHead5);
+	table.appendChild(tableHead6);
+	table.appendChild(tableHead7);
+	table.appendChild(tableHead8);
+	table.appendChild(tableHead9);
+
+	for (let i = 0; i < eventList.length; i++) {
+
+		// table data
+		let tableRow = document.createElement('tr');
+		table.appendChild(tableRow);
+
+		let tableData1 = document.createElement('td');
+		// let tableData2 = document.createElement('td');
+		let tableData3 = document.createElement('td');
+		let tableData4 = document.createElement('td');
+		let tableData5 = document.createElement('td');
+		let tableData6 = document.createElement('td');
+		let tableData7 = document.createElement('td');
+		let tableData8 = document.createElement('td');
+		let tableData9 = document.createElement('td');
+		tableData1.textContent = eventList[i].name;
+		// tableData2.textContent = eventList[i].description;
+		tableData3.textContent = eventList[i].developer;
+		tableData4.textContent = eventList[i].platform;
+		tableData5.textContent = eventList[i].releaseDate;
+		tableData6.textContent = eventList[i].completed;
+		tableData7.textContent = eventList[i].hoursToComplete;
+		tableData8.textContent = eventList[i].imgUrl;
+		tableData9.textContent = eventList[i].genre;
+		tableRow.appendChild(tableData1);
+		// tableRow.appendChild(tableData2);
+		tableRow.appendChild(tableData3);
+		tableRow.appendChild(tableData4);
+		tableRow.appendChild(tableData5);
+		tableRow.appendChild(tableData6);
+		tableRow.appendChild(tableData7);
+		tableRow.appendChild(tableData8);
+		tableRow.appendChild(tableData9);
+	}
+
+	dataDiv.appendChild(table);
+	document.body.appendChild(dataDiv);
+
+	// for(let i = 0; i < eventList.length; i++){
+	// let eventH2 = document.createElement('h2');
+	// dataDiv.appendChild(eventH2);
+	// eventH2.textContent = eventList[i].name;
+	//		
+	// let descriptionBq = document.createElement('blockquote');
+	// dataDiv.appendChild(descriptionBq);
+	// descriptionBq.textContent = eventList[i].description;
+	//		
+	// let miscUl = document.createElement('ul');
+	// dataDiv.appendChild(miscUl);
+	//		
+	// let developerLi = document.createElement('li');
+	// miscUl.appendChild(developerLi);
+	// developerLi.textContent = 'Developer: ' + eventList[i].developer;
+	//		
+	// let platformLi = document.createElement('li');
+	// miscUl.appendChild(platformLi);
+	// platformLi.textContent = 'Platform: ' + eventList[i].platform;
+	//		
+	// let releaseDateLi = document.createElement('li');
+	// miscUl.appendChild(releaseDateLi);
+	// releaseDateLi.textContent = 'Release Date: ' + eventList[i].releaseDate;
+	//		
+	// let completedLi = document.createElement('li');
+	// miscUl.appendChild(completedLi);
+	// if(eventList[i].completed){
+	// completedLi.textContent = 'Completed: Yes';
+	// }
+	// else{
+	// completedLi.textContent = 'Completed: No';
+	//
+	// }
+	//		
+	// let hoursToCompleteLi = document.createElement('li');
+	// miscUl.appendChild(hoursToCompleteLi);
+	// hoursToCompleteLi.textContent = 'Hours To Complete: ' +
+	// eventList[i].hoursToComplete;
+	//		
+	// let imgUrlLi = document.createElement('li');
+	// miscUl.appendChild(imgUrlLi);
+	// imgUrlLi.textContent = 'IMG URL: ' + eventList[i].imgUrl;
+	//		
+	// let genreLi = document.createElement('li');
+	// miscUl.appendChild(genreLi);
+	// genreLi.textContent = 'Genre: ' + eventList[i].genre;
+	//		
+	// }
 
 	console.log(eventList);
 }
@@ -229,51 +244,50 @@ function displayEvent(event) {
 	while (dataDiv.firstElementChild) {
 		dataDiv.removeChild(dataDiv.firstElementChild);
 	}
-	
-	
+
 	let eventH2 = document.createElement('h2');
 	dataDiv.appendChild(eventH2);
-	eventH2.textContent = eventList[i].name;
-	
+	eventH2.textContent = event.name;
+
 	let descriptionBq = document.createElement('blockquote');
 	dataDiv.appendChild(descriptionBq);
-	descriptionBq.textContent = eventList[i].description;
-	
+	descriptionBq.textContent = event.description;
+
 	let miscUl = document.createElement('ul');
 	dataDiv.appendChild(miscUl);
-	
+
 	let developerLi = document.createElement('li');
 	miscUl.appendChild(developerLi);
-	developerLi.textContent = 'Developer: ' + eventList[i].developer;
-	
+	developerLi.textContent = 'Developer: ' + event.developer;
+
 	let platformLi = document.createElement('li');
 	miscUl.appendChild(platformLi);
-	platformLi.textContent = 'Platform: ' + eventList[i].platform;
-	
+	platformLi.textContent = 'Platform: ' + event.platform;
+
 	let releaseDateLi = document.createElement('li');
 	miscUl.appendChild(releaseDateLi);
-	releaseDateLi.textContent = 'Release Date: ' + eventList[i].releaseDate;
-	
+	releaseDateLi.textContent = 'Release Date: ' + event.releaseDate;
+
 	let completedLi = document.createElement('li');
 	miscUl.appendChild(completedLi);
-	if(eventList[i].completed){
+	if (event.completed) {
 		completedLi.textContent = 'Completed: Yes';
-	}
-	else{
+	} else {
 		completedLi.textContent = 'Completed: No';
 
 	}
-	
+
 	let hoursToCompleteLi = document.createElement('li');
 	miscUl.appendChild(hoursToCompleteLi);
-	hoursToCompleteLi.textContent = 'Hours To Complete: ' + eventList[i].hoursToComplete;
-	
+	hoursToCompleteLi.textContent = 'Hours To Complete: '
+			+ event.hoursToComplete;
+
 	let imgUrlLi = document.createElement('li');
 	miscUl.appendChild(imgUrlLi);
-	imgUrlLi.textContent = 'IMG URL: ' + eventList[i].imgUrl;
-	
+	imgUrlLi.textContent = 'IMG URL: ' + event.imgUrl;
+
 	let genreLi = document.createElement('li');
 	miscUl.appendChild(genreLi);
-	genreLi.textContent = 'Genre: ' + eventList[i].genre;
-	
+	genreLi.textContent = 'Genre: ' + event.genre;
+
 }
